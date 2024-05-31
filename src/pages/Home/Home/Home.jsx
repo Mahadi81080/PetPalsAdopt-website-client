5fc3923aa6ab74e77e8d7780018addd7143e6f26
Mahadi810803 <mahedihasan81080@gmail.com>
2024-06-01 01:59:14 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { NavbarDark } from "../../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <NavbarDark></NavbarDark>
+      <h2>This is home section</h2>
+    </div>
+  );
+};
+
+export default Home;
